4a8fec656ba741d65dc100dd00b35bbbef071f67
Michael Lai <themichaellai@gmail.com>
2013-02-23 17:56:43 -0500

    Fixed duplicate listings

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 var socket = io.connect(window.location.hostname);
-var messagesList = $('tbody');
 
 socket.on('message', function (data) {
-  $('tr').remove();
+  var messagesList = $('tbody');
+  messagesList.find('tr').remove();
   now = Date.now();
   $('h1').text(data.coming);
+  console.log(data.closest);
   for (var i = 0; i < data.closest.length; i++) {
     messagesList.append('<tr><td>' + data.closest[i].name + '</td><td>' + data.closest[i].delta + '</td></tr></li>');
   }
